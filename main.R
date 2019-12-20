3c19690554cd0b7b4679f61b7494d8ee8cff11db
GolDandy7 <conidi.andrea7@gmail.com>
2019-12-20 18:02:01 +0100

    aggiunto un file R per fare la bind del file C.txt alle altre features

--- main.R
+++ main.R
@@ -11,5 +11,13 @@ df_x <- read.table("X.txt",header = FALSE)
 df_c <- read.table("C.txt",header = FALSE)
 df_y <- read.table("Y.txt",header = FALSE)
 
+#trasformo le C in una matrice  di 0 e 1 perchè la numerazione mi crea ordinamento
+my_c_data<-new_c_data(df_c)
 
-my_data<-data_split(df_x)
+#my_data contenente i dati splittati
+my_data<-data_split(new_data)
+
+#scegliamo il classificatore in cross-validation
+#dati già normalizzati in ingresso
+#cross_validation_output<-choosing_classifier(my_data)
+
