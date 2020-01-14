45a1f6277e422956beb2b29e82858013475598d6
GolDandy7 <conidi.andrea7@gmail.com>
2020-01-14 14:17:28 +0100

    aggiunte altre features : occorrenze zero,area negativa e positiva

--- feature_selection.R
+++ feature_selection.R
@@ -1,3 +1,4 @@
+#misura il tempo in cui il segnale è crescente
 rising_time <- function(values) {
   
   total_time = 0
@@ -11,19 +12,28 @@ rising_time <- function(values) {
   return(total_time)
 }
 
-#numero di elementi positivi per riga di uno specifico segnale
-positive_values <- function(values) {
+#conta il numero di occorrenze per zero della funzione
+crossing_zero <- function(values) {
   
   counter = 0
   k<-length(values)
   k<-k-1
   for (i in seq_len(k)) {
-    if (values[i] >= 0) {
-      counter = counter + 1
+    if (values[i] >=0) {
+      if(values[i+1]<0){
+        counter = counter + 1
+      }
+    }else{
+      if(values[i+1]>0){
+        counter = counter + 1
+      }
     }
   }
   return(counter)
+  
 }
+
+#misura l'area totale del segnale
 compute_area<-function(channel_data){
   
   #determino la dimensione del channel(nel nostro caso deve essere di 204)
@@ -33,32 +43,27 @@ compute_area<-function(channel_data){
   return(area)
 }
 
-new_features<-function(df_x){
+#misura l'area negativa del segnale
+compute_negative_area <-function(channel_data){
   
-  channel_area_names<-c("Fz_Area", "Cz_Area", "Pz_Area", "Oz_Area", "P3_Area", "P4_Area", "PO7_Area", "PO8_Area")
-  channel_rt_names<-c("Fz_RT", "Cz_RT", "Pz_RT", "Oz_RT", "P3_RT", "P4_RT", "PO7_RT", "PO8_RT")
-  
-  num_channel<-8
-  size<-ncol(df_x)/num_channel
-  features<-dataFrame(matrix(ncol = 16, nrow = 0))
-  for(i in seq_len(num_channel)){
-    
-    start<-((i-1)*size)+1
-    end<-i*size
-    channel<-df_x[,c(start:end)]
-    feature_area<-apply(channel, 1,compute_area)
-    feature_area<-as.data.frame(feature_area)
-    colnames(feature_area)<-channel_area_names[i]
-    features<-cbind(features,feature_area)
-    feature_rt<-apply(channel, 1, rising_time)
-    feature_rt<-as.data.frame(feature_rt)
-    names(feature_rt)<-channel_rt_names[i]
-    
-  }
-  return(features)
+  #determino la dimensione del channel(nel nostro caso deve essere di 204)
+  dim<-length(channel_data)
+  area<-AUC(x=c(1:dim),y=pmin(0,channel_data),method = "trapezoid")
+  return(area)
   
 }
 
+#misura l'area positiva del segnale
+compute_positive_area <-function(channel_data){
+  
+  #determino la dimensione del channel(nel nostro caso deve essere di 204)
+  dim<-length(channel_data)
+  area<-AUC(x=c(1:dim),y=pmax(0,(channel_data)),method = "trapezoid")
+  return(area)
+  
+}
+
+#conta il numero di valori positivi --> non serve
 features_positive<-function(df_x){
   
   channel_label<-c("Fz_+", "Cz_+", "Pz_+", "Oz_+", "P3_+", "P4_+", "PO7_+", "PO8_+")
@@ -76,9 +81,11 @@ features_positive<-function(df_x){
     features_positives[,i]<-feature_positive
   }
   colnames(features_positives)<-channel_label
-  features_positives<-as.data.frame(features_positives)
+  features_positives <-as.data.frame(features_positives)
   return(features_positives)
 }
+
+#misura  il tempo di salita del segnale
 features_rt_channel<-function(df_x){
   
   channel_rt_names<-c("Fz_RT", "Cz_RT", "Pz_RT", "Oz_RT", "P3_RT", "P4_RT", "PO7_RT", "PO8_RT")
@@ -88,7 +95,7 @@ features_rt_channel<-function(df_x){
   
   
   for(i in seq_len(num_channel)){
-    printf("Iterazione: %d\n",i)
+    #printf("Iterazione: %d\n",i)
     start<-((i-1)*size)+1
     end<-i*size
     channel<-df_x[,c(start:end)]
@@ -100,6 +107,7 @@ features_rt_channel<-function(df_x){
   return(features_rt)
 }
 
+#calcola il valore del modulo dell'area
 features_area_channel<-function(df_x){
   
   channel_label<-c("Fz_Area", "Cz_Area", "Pz_Area", "Oz_Area", "P3_Area", "P4_Area", "PO7_Area", "PO8_Area")
@@ -109,7 +117,7 @@ features_area_channel<-function(df_x){
   
   
   for(i in seq_len(num_channel)){
-   printf("Iterazione: %d\n",i)
+   #printf("Iterazione: %d\n",i)
    start<-((i-1)*size)+1
    end<-i*size
    channel<-df_x[,c(start:end)]
@@ -119,4 +127,114 @@ features_area_channel<-function(df_x){
   colnames(features_area)<-channel_label
   features_area<-as.data.frame(features_area)
   return(features_area)
+}
+
+#calcola il valore dell'area positiva
+features_positive_area_channel<-function(df_x){
+  
+  channel_label<-c("Fz_Area+", "Cz_Area+", "Pz_Area+", "Oz_Area+", "P3_Area+", "P4_Area+", "PO7_Area+", "PO8_Area+")
+  num_channel<-8
+  size<-ncol(df_x)/num_channel
+  features_area<-(matrix(ncol = num_channel,nrow = nrow(df_x)))
+  
+  
+  for(i in seq_len(num_channel)){
+    #printf("Iterazione: %d\n",i)
+    start<-((i-1)*size)+1
+    end<-i*size
+    channel<-df_x[,c(start:end)]
+    feature_area<-apply(channel, 1,compute_positive_area)
+    features_area[,i]<-feature_area
+  }
+  colnames(features_area)<-channel_label
+  features_area<-as.data.frame(features_area)
+  return(features_area)
+}
+
+#calcola il valore dell'area negativa
+features_negative_area_channel<-function(df_x){
+  
+  channel_label<-c("Fz_Area+", "Cz_Area-", "Pz_Area-", "Oz_Area-", "P3_Area-", "P4_Area-", "PO7_Area-", "PO8_Area-")
+  num_channel<-8
+  size<-ncol(df_x)/num_channel
+  features_area<-(matrix(ncol = num_channel,nrow = nrow(df_x)))
+  
+  
+  for(i in seq_len(num_channel)){
+    #printf("Iterazione: %d\n",i)
+    start<-((i-1)*size)+1
+    end<-i*size
+    channel<-df_x[,c(start:end)]
+    feature_area<-apply(channel, 1,compute_negative_area)
+    features_area[,i]<-feature_area
+  }
+  colnames(features_area)<-channel_label
+  features_area<-as.data.frame(features_area)
+  return(features_area)
+}
+#calcola il valore di picco del segnale x canale
+features_peak<-function(df_x){
+  
+  channel_label<-c("Fz_peak", "Cz_peak", "Pz_peak", "Oz_peak", "P3_peak", "P4_peak", "PO7_peak", "PO8_peak")
+  num_channel<-8
+  size<-ncol(df_x)/num_channel
+  features_peak_values<-(matrix(ncol = num_channel,nrow = nrow(df_x)))
+  
+  
+  for(i in seq_len(num_channel)){
+    #printf("Iterazione: %d\n",i)
+    start<-((i-1)*size)+1
+    end<-i*size
+    channel<-df_x[,c(start:end)]
+    feature<-apply(channel, 1,max)
+    features_peak_values[,i]<-feature
+  }
+  colnames(features_peak_values)<-channel_label
+  features_peak_values <-as.data.frame(features_peak_values)
+  return(features_peak_values)
+}
+
+#calcola il valore di picco picco del segnale x canale
+features_peak_to_peak<-function(df_x){
+  
+  channel_label<-c("Fz_PPeak", "Cz_PPeak", "Pz_PPeak", "Oz_PPeak", "P3_PPeak", "P4_PPeak", "PO7_PPeak", "PO8_PPeak")
+  num_channel<-8
+  size<-ncol(df_x)/num_channel
+  features_Ppeak_values<-(matrix(ncol = num_channel,nrow = nrow(df_x)))
+  
+  
+  for(i in seq_len(num_channel)){
+    #printf("Iterazione: %d\n",i)
+    start<-((i-1)*size)+1
+    end<-i*size
+    channel<-df_x[,c(start:end)]
+    max_peak<- apply(channel, 1,max)
+    min_peak<- apply(channel,1,min)
+    features_Ppeak_values[,i]<-(max_peak-min_peak)
+  }
+  colnames(features_Ppeak_values)<-channel_label
+  features_Ppeak_values <-as.data.frame(features_Ppeak_values)
+  return(features_Ppeak_values)
+}
+
+#calcola quante volte il segnale passa per zero
+features_crossing_zero<-function(df_x){
+  
+  channel_label<-c("Fz_Cross0", "Cz_Cross0", "Pz_Cross0", "Oz_Cross0", "P3_Cross0", "P4_Cross0", "PO7_Cross0", "PO8_Cross0")
+  num_channel<-8
+  size<-ncol(df_x)/num_channel
+  features_czero_values<-(matrix(ncol = num_channel,nrow = nrow(df_x)))
+  
+  
+  for(i in seq_len(num_channel)){
+    #printf("Iterazione: %d\n",i)
+    start<-((i-1)*size)+1
+    end<-i*size
+    channel<-df_x[,c(start:end)]
+    c_zero<- apply(channel,1,crossing_zero)
+    features_czero_values[,i]<-c_zero
+  }
+  colnames(features_czero_values)<-channel_label
+  features_czero_values <-as.data.frame(features_czero_values)
+  return(features_czero_values)
 }
--- main.R
+++ main.R
@@ -26,20 +26,33 @@ df_y <- read.table("Y.txt",header = FALSE)
 #Applicazione delle etichette sulle colonne del df
 df_x <- apply_labels(df_x)
 
+#set del seme per la ripetibilità dell'esperimento
+set.seed(123)
+
+
 #------------features---------------------
-#features_area<-features_area_channel(df_x)
+features_area<-features_area_channel(df_x)
+features_area_positive <-features_positive_area_channel(df_x)
+features_area_negative <-features_negative_area_channel(df_x)
 features_rt<-features_rt_channel(df_x)
-#feature_p<-features_positive(df_x)
+features_cz<-features_crossing_zero(df_x)
+feature_pp<-features_peak_to_peak(df_x)
+
 #se aggiungo feature_p peggioro in cross validation e rimango uguale sul test
 #-----------------------------------------
 #trasformo le C in una matrice  di 0 e 1 perchè la numerazione mi crea ordinamento
 my_c_data<-new_c_data(df_c)
-new_data<-cbind(df_c, df_x, my_c_data, features_area,features_rt, feature_p, df_y)
+new_data<-cbind(df_c, df_x, my_c_data)
+new_data<-cbind(new_data,features_area)
+new_data<-cbind(new_data,features_rt)
+new_data<-cbind(new_data,features_cz)
+new_data<-cbind(new_data,features_area_negative)
+new_data<-cbind(new_data,features_area_positive)
+new_data<-cbind(new_data,feature_pp)
+new_data<-cbind(new_data,df_y)
 
 #new_data<-cbind(df_x, df_y)
 
-#set del seme per la ripetibilità dell'esperimento
-set.seed(123)
 
 #split dei dati in training e test: data$train e data$test 
 data<-data_split(new_data)
